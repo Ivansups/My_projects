33d9111209bdccf279be65a3490e2f5fc2ce9ea0
Ivansups <156587029+Ivansups@users.noreply.github.com>
2025-09-18 09:43:57 +0300

    dev task 2

--- C++_lessons/taskTwo/src/main.cpp
+++ C++_lessons/taskTwo/src/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void transpose(int x, int y) {
-    int trans[y][x];
-    cout << "Транспонированная матрица:" << endl;
+int validate() {
+    int number;
+    while (true) {
+        cin >> number;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Ошибка: введите целое число!" << endl;
+            continue;
+        }
+        break;
+    }
+    return number;
+}
+
+void transpose(string** arr, int x, int y) {
+    cout << "\nТранспонированная матрица:" << endl;
     for (int i = 0; i < y; i++) {
         for (int j = 0; j < x; j++) {
-            cout << trans[j][i] << " ";
+            cout << arr[j][i] << " ";
         }
         cout << endl;
     }
@@ -14,21 +29,41 @@ void transpose(int x, int y) {
 
 int main() {
     int x, y;
-    cout << "Введите x (количество строк): ";
-    cin >> x;
-    cout << "Введите y (количество столбцов): ";
-    cin >> y;
     
-    int arr[x][y]; // Простой массив
+    cout << "Введите x (количество строк): ";
+    x = validate();
+    
+    cout << "Введите y (количество столбцов): ";
+    y = validate();
+    
+    string** arr = new string*[x];
+    for(int i = 0; i < x; i++) {
+        arr[i] = new string[y];
+    }
     
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < y; j++) {
-            cout << "Введите arr[" << i << "][" << j << "] (элемент матрицы): ";
+            cout << "Введите arr[" << i << "][" << j << "]: ";
             cin >> arr[i][j];
         }
     }
+
+    cout << "\nИсходная матрица:" << endl;
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     
-    transpose(x, y);
+    transpose(arr, x, y);
+    
+    for(int i = 0; i < x; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+    
     return 0;
 }
 
